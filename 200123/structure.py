73377cf685a2e0716634c3521129470bc14698f5
eann.s.yoon <eann.s.yoon@gmail.com>
2020-01-23 11:58:33 +0900

    200123

---
+++ 200123/structure.py
@@ -0,0 +1,5 @@
+print( [1, "paris", -9.8, 3>5 , (2+2)*2 ] )
+print( [1, "paris", -9.8, 3>5 , (2+2)*2 ][3]   )
+print( [1, "paris", -9.8, 3>5 , (2+2)*2 ][0]   )
+print( ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"] )
+print( ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"][2] )
